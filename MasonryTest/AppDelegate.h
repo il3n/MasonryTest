6d34858d2c8e0823d41ecf332797f5771193645a
jarvis.lee <448754807@qq.com>
2016-07-27 16:04:36 +0800

    init

---
+++ MasonryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryTest
+//
+//  Created by lijun on 16/7/27.
+//  Copyright © 2016年 Youku.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasonryTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MasonryTest
+//
+//  Created by lijun on 16/7/27.
+//  Copyright © 2016年 Youku.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
